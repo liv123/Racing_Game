7bc7dd32edef24ac6269964bd9dc38baf3dabf2f
liv123 <olivia.e@gmail.com>
2015-08-16 21:50:42 -0700

    added functions to js

--- js/Racing_Game.js
+++ js/Racing_Game.js
@@ -1,19 +1,30 @@
-
+var i = 0
 
 function hitKey() {
-//when player presses key move fwd 1 until end is reached
-  alert('key pressed')
+//when player hits key: move fwd 1 until end is reached
+  if (i < 4) {
+     $(".active").removeClass("active").next().addClass("active");
+     i = i+1;
+  }
+  else{
+      alert("player 1 wins!");   
+    
+  }
 } 
 
 function play() {
-  // when play pressed, clear board and start in cell 1
-  $("table td:first-child").css("background-color", "red");
+  // when play pressed, clear board and start in cell 1 
+  $("table td").removeClass("active");
+  $("table td:first-child").addClass("active");
+  i=0;
 }
 
 $(document).ready(function() {
   //attach listeners
   $(".play").click(play);
-  $("hitKey").keypress();
+  $(document).keypress(hitKey);
 
 
 });
+
+
