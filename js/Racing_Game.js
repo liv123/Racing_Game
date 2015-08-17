3467a58ced138df99cbcd4fa2de7509df76fbcf1
liv123 <olivia.e@gmail.com>
2015-08-16 23:02:37 -0700

    bigger track

--- js/Racing_Game.js
+++ js/Racing_Game.js
@@ -1,29 +1,30 @@
-var i = 0
-var x = 0
+var i = 1;
+var x = 1;
+var tracksize = 10;
 
 function hitKey(e) {
 
 
 //when player hits key: move fwd 1 until end is reached
   if (e.which == '97') { 
-    if (i < 4) {
+    if (i < tracksize) {
        $("#player1 .active").removeClass("active").next().addClass("active");
        i = i+1;
     }
     else {
-      if (x<4) {
+      if (x<tracksize) {
         alert("player 1 wins!");  
       }
     }
   }
 
 else if (e.which == '108') { 
-    if (x < 4) {
+    if (x < tracksize) {
        $("#player2 .active").removeClass("active").next().addClass("active");
        x = x+1;
     }
     else {
-      if (i<4) {
+      if (i < tracksize) {
         alert("player 2 wins!");  
       }
     }
@@ -44,8 +45,8 @@ function play() {
   // when play pressed, clear board and start in cell 1 
   $("table td").removeClass("active");
   $("table td:first-child").addClass("active");
-  i=0;
-  x=0;
+  i=1;
+  x=1;
 }
 
 $(document).ready(function() {
